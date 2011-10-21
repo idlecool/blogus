f9b5516cda41a4e9af4a51489a2ca8978be39129
Shiv Deepak <idlecool@gmail.com>
2011-10-21 16:15:08 +0530

    deploy script added and finished

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ Blogus Config File
 
 site_name = "Shiv Deepak"
 site_title = "Shiv Deepak (idlecool)"
-site_path = "http://www.blogus.com"
-site_cdn_path = "http://cdn.blogus.com"
-site_cookie_path = "blogus.com"
+site_path = "http://www.blogus.com" 
+site_cdn_path = "http://cdn.blogus.com" 
+site_cookie_path = "blogus.com" 
 
 import os
 INSTALLATION_PATH = os.path.dirname(os.path.abspath(__file__))
--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 #Local Configuration
 LOCAL_URL="http://www.blogus.com"
 LOCAL_CDN_URL="http://cdn.blogus.com"
+LOCAL_COOKIE_DOMAIN="blogus.com"
 
 #Server Configuration
 DEPLOY_URL="http://beta.idlecool.net"
+DEPLOY_COOKIE_DOMAIN="idlecool.net"
 DEPLOY_USERNAME="m"
 DEPLOY_CDN_URL="http://cdn.idlecool.net"
 DEPLOY_HOST_NAME="ntra.in"
@@ -22,8 +24,20 @@ function alert {
     echo
 }
 
+alert "Generating Deployment Configuration"
+sed 's/^site_path = .*/site_path = \"'"$(echo $DEPLOY_URL | sed -e 's/\//\\\//g')"'\" /' <config.py >config.py.new.1
+sed 's/^site_cdn_path = .*/site_cdn_path = \"'"$(echo $DEPLOY_CDN_URL | sed -e 's/\//\\\//g')"'\" /' <config.py.new.1 >config.py.new.2
+sed 's/^site_cookie_path = .*/site_cookie_path = \"'"$(echo $DEPLOY_COOKIE_DOMAIN | sed -e 's/\//\\\//g')"'\" /' <config.py.new.2 >config.py
+rm config.py.new.1 config.py.new.2
+
 alert "Initiating rsync: Syncing With The Server"
 rsync -r -a -v --exclude=LICENSE --exclude=deploy.sh --exclude=logs --exclude=cdn --exclude=app.yaml --exclude=.* -e "ssh -l "$DEPLOY_USERNAME --delete . $DEPLOY_HOST_NAME:$DEPLOY_WSGI_DIR
+
+alert "Restoring Local Configuration"
+sed 's/^site_path = .*/site_path = \"'"$(echo $LOCAL_URL | sed -e 's/\//\\\//g')"'\" /' <config.py >config.py.old.1
+sed 's/^site_cdn_path = .*/site_cdn_path = \"'"$(echo $LOCAL_CDN_URL | sed -e 's/\//\\\//g')"'\" /' <config.py.old.1 >config.py.old.2
+sed 's/^site_cookie_path = .*/site_cookie_path = \"'"$(echo $LOCAL_COOKIE_DOMAIN | sed -e 's/\//\\\//g')"'\" /' <config.py.old.2 >config.py
+rm config.py.old.1 config.py.old.2
 
 alert "Syncing the CDN"
 rsync -avr --delete cdn/ $GAE_APP_DIR"root"
